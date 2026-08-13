c019ab0daaec1b1a2112a9e4b81f8aa6744d9f40
Tuna <safiryakutelmas@gmail.com>
2026-08-13 17:44:58 +0300

    header 13.08

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-screen bg-white text-slate-900 antialiased">
         <Header />
         <main className="flex-1 pt-16">{children}</main>
       </body>
